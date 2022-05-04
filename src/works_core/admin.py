f2f4d9622390bd5e425a1911d31dd0d6e96c16c4
facetable-lab <diofff6@gmail.com>
2022-05-04 07:40:58 +0300

    Добавил комментарии (документацию)

--- src/works_core/admin.py
+++ src/works_core/admin.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 
 from works_core.models import *
 
+# Регистрация модели City
 admin.site.register(City)
+
+# Регистрация модели Specialization
 admin.site.register(Specialization)
