4dca286fe52d2f002b313f4f2811617c5fcc0cb3
facetable-lab <diofff6@gmail.com>
2022-05-04 07:41:09 +0300

    Добавил комментарии (документацию)

--- src/works_core/apps.py
+++ src/works_core/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
 
 
+# Класс с настройками приложения
 class WorksCoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'works_core'
