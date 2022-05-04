16099c544870ae23882f80f813ad170872240810
facetable-lab <diofff6@gmail.com>
2022-05-04 08:34:27 +0300

    Добавил комментарии

--- src/works_core/views.py
+++ src/works_core/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from .models import Vacancy
 
 
+# Функция отображения главной страницы
 def home_view(request):
     vacancy_all = Vacancy.objects.all()
     context = {
