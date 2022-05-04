9006b0062e28fe2c444657737314920ac87bc487
facetable-lab <diofff6@gmail.com>
2022-05-04 07:14:39 +0300

    Новая модель специализации (ЯП)

--- src/works_core/models.py
+++ src/works_core/models.py
@@ -2,12 +2,24 @@ from django.db import models
 
 
 class City(models.Model):
-    name = models.CharField(max_length=25, verbose_name='Название населенного пункта')
-    slug = models.CharField(max_length=40, blank=True)
+    name = models.CharField(max_length=25, verbose_name='Название населенного пункта', unique=True)
+    slug = models.CharField(max_length=40, blank=True, unique=True)
 
     class Meta:
         verbose_name = 'Название населенного пункта'
         verbose_name_plural = 'Название населенных пунктов'
+
+    def __str__(self):
+        return self.name
+
+
+class Specialization(models.Model):
+    name = models.CharField(max_length=25, verbose_name='Язык программирования', unique=True)
+    slug = models.CharField(max_length=40, blank=True, unique=True)
+
+    class Meta:
+        verbose_name = 'Язык программирования'
+        verbose_name_plural = 'Языки программирования'
 
     def __str__(self):
         return self.name
