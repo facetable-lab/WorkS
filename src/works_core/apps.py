2577d546ebf1a7aed121c86e17cd279f98192fd2
facetable-lab <diofff6@gmail.com>
2022-05-04 07:09:11 +0300

    Русифицировал название приложения в админке

--- src/works_core/apps.py
+++ src/works_core/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class WorksCoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'works_core'
+    verbose_name = 'Ядро сайта (приложение по сбору вакансий)'
