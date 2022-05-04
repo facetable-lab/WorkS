b25b9bb5f899d9de21dcc101544555b7959dec58
facetable-lab <diofff6@gmail.com>
2022-05-05 00:54:20 +0300

    Оформление формы

--- src/works_core/forms.py
+++ src/works_core/forms.py
@@ -4,5 +4,11 @@ from works_core.models import City, Specialization
 
 
 class FindForm(forms.Form):
-   city = forms.ModelChoiceField(queryset=City.objects.all())
-   specialization = forms.ModelChoiceField(queryset=Specialization.objects.all())
+    city = forms.ModelChoiceField(queryset=City.objects.all(), to_field_name='slug', required=False,
+                                  widget=forms.Select(attrs={
+                                      'class': 'form-control'
+                                  }), label='Город')
+    specialization = forms.ModelChoiceField(queryset=Specialization.objects.all(), to_field_name='slug', required=False,
+                                            widget=forms.Select(attrs={
+                                                'class': 'form-control'
+                                            }), label='Специальность')
