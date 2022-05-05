23a1ed552316cca7af45ba14756b9829048f3326
facetable-lab <diofff6@gmail.com>
2022-05-05 03:36:32 +0300

    Новая модель создания пользователя

--- src/works_accounts/models.py
+++ src/works_accounts/models.py
@@ -1,10 +1,33 @@
-from django.contrib.auth.base_user import AbstractBaseUser
-from django.contrib.auth.models import UserManager
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
 from django.db import models
 
 
-# Модель пользователя (подписчика)
-class MyUser(AbstractBaseUser):
+# Модель для создания пользователя (подписчика)
+class SubscriberManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None):
+        user = self.create_user(
+            email,
+            password=password,
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+# Модель настроек пользователя (подписчика)
+class Subscriber(AbstractBaseUser):
     email = models.EmailField(
         verbose_name='email address',
         max_length=255,
@@ -12,11 +35,11 @@ class MyUser(AbstractBaseUser):
     )
     is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
-    city = models.ForeignKey('City', on_delete=models.SET_NULL, null=True, blank=True)
-    specialization = models.ForeignKey('Specialization', on_delete=models.SET_NULL, null=True, blank=True)
+    city = models.ForeignKey('works_core.City', on_delete=models.SET_NULL, null=True, blank=True)
+    specialization = models.ForeignKey('works_core.Specialization', on_delete=models.SET_NULL, null=True, blank=True)
     is_mailing = models.BooleanField(default=True)
 
-    objects = UserManager()
+    objects = SubscriberManager()
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
