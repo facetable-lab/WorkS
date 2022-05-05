7aa844c2bd310d1991c6c0ffa24d1a4e46e53db0
facetable-lab <diofff6@gmail.com>
2022-05-05 04:08:28 +0300

    Отображение в админке

--- src/works_accounts/apps.py
+++ src/works_accounts/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class WorksAccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'works_accounts'
+    verbose_name = 'Пользователи'
