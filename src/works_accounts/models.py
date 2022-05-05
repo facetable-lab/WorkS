6c3015c939c875e1e5a6ddf864f75c2a2248e71c
facetable-lab <diofff6@gmail.com>
2022-05-05 03:17:43 +0300

    Новая модель приложения works_accounts, для пользователя сайта

--- src/works_accounts/models.py
+++ src/works_accounts/models.py
@@ -1,3 +1,35 @@
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import UserManager
 from django.db import models
 
-# Create your models here.
+
+# Модель пользователя (подписчика)
+class MyUser(AbstractBaseUser):
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True,
+    )
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    city = models.ForeignKey('City', on_delete=models.SET_NULL, null=True, blank=True)
+    specialization = models.ForeignKey('Specialization', on_delete=models.SET_NULL, null=True, blank=True)
+    is_mailing = models.BooleanField(default=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.is_admin
