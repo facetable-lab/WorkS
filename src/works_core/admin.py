ba3b336711b4064648a50cf2d428530857b102f1
facetable-lab <diofff6@gmail.com>
2022-05-05 04:08:19 +0300

    Отображение в админке

--- src/works_core/admin.py
+++ src/works_core/admin.py
@@ -2,11 +2,24 @@ from django.contrib import admin
 
 from works_core.models import *
 
+
+class SpecializationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+
+
+class CityAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+
+
+class VacancyAdmin(admin.ModelAdmin):
+    list_display = ('title', 'city', 'specialization', 'company')
+
+
 # Регистрация модели City
-admin.site.register(City)
+admin.site.register(City, CityAdmin)
 
 # Регистрация модели Specialization
-admin.site.register(Specialization)
+admin.site.register(Specialization, SpecializationAdmin)
 
 # Регистрация модели Vacancy
-admin.site.register(Vacancy)
+admin.site.register(Vacancy, VacancyAdmin)
