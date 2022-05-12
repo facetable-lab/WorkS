71f48d5c8a60eecf14f07e13611de31c42caa53b
facetable-lab <diofff6@gmail.com>
2022-05-13 01:20:46 +0300

    Парсер v2 Updates: - Основной функционал разделен по функциям - Для каждого сайта своя функция - Добавлен сбор ошибок для анализа - Добавлен сайт rabota.ru

--- src/work.py
+++ src/work.py
@@ -7,35 +7,78 @@ headers = {'User-Agent': 'Mozilla/5.0 (Windows WorkS 13.37228; rv:47.0) Gecko/20
            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*,q=0.8'
            }
 
-url = 'https://rostov.hh.ru/search/vacancy?text=python&from=suggest_post&fromSearchLine=true&area=76'
-resp = requests.get(url, headers=headers)
-jobs = []
-errors = []
-if resp.status_code == 200:
-    soup = bs(resp.content, 'html.parser')
-    main_div = soup.find('div', attrs={'class': 'vacancy-serp-content'})  # class_={"class": "style"}
-    if main_div:
-        div_list = main_div.find_all('div', attrs={'class': 'vacancy-serp-item'})
-        for div in div_list:
-            title = div.find('h3')
-            href = title.a['href']
-            div_two_span = div.find('div', attrs={'class': 'g-user-content'})
-            content_part_1 = div_two_span.find('div', attrs={'class': 'bloko-text'}).text
-            content_part_2 = div_two_span.find('div', attrs={'class': 'bloko-text bloko-text_no-top-indent'}).text
-            content = content_part_1 + content_part_2
-            company = 'No name'
-            cname_1 = div.find('div', attrs={'class': 'vacancy-serp-item__meta-info-company'})
-            if cname_1:
-                company = cname_1.a.text
-            jobs.append({'title': title.text,
-                         'url': href,
-                         'description': content,
-                         'company': company})
-    else:
-        errors.append({'url': url, 'title': 'Div does not exists'})
-else:
-    errors.append({'url': url, 'title': 'Page do not response'})
 
-h = codecs.open('work.txt', 'w', 'utf-8')
-h.write(str(jobs))
-h.close()
+def hh_ru(url):
+    jobs = []
+    errors = []
+    url = 'https://rostov.hh.ru/search/vacancy?text=python&from=suggest_post&fromSearchLine=true&area=76'
+    resp = requests.get(url, headers=headers)
+    if resp.status_code == 200:
+        soup = bs(resp.content, 'html.parser')
+        main_div = soup.find('div', attrs={'class': 'vacancy-serp-content'})  # class_={"class": "style"}
+        if main_div:
+            div_list = main_div.find_all('div', attrs={'class': 'vacancy-serp-item'})
+            for div in div_list:
+                title = div.find('h3')
+                href = title.a['href']
+                div_two_span = div.find('div', attrs={'class': 'g-user-content'})
+                content_part_1 = div_two_span.find('div', attrs={'class': 'bloko-text'}).text
+                content_part_2 = div_two_span.find('div', attrs={'class': 'bloko-text bloko-text_no-top-indent'}).text
+                content = content_part_1 + content_part_2
+                company = 'No name'
+                cname_1 = div.find('div', attrs={'class': 'vacancy-serp-item__meta-info-company'})
+                if cname_1:
+                    company = cname_1.a.text
+                jobs.append({'title': title.text,
+                             'url': href,
+                             'description': content,
+                             'company': company})
+        else:
+            errors.append({'url': url, 'title': 'Div does not exists'})
+    else:
+        errors.append({'url': url, 'title': 'Page do not response'})
+
+    return jobs, errors
+
+
+def rabota_ru(url):
+    domain = 'https://rostov.rabota.ru'
+    jobs = []
+    errors = []
+    resp = requests.get(url, headers=headers)
+    if resp.status_code == 200:
+        soup = bs(resp.content, 'html.parser')
+        main_div = soup.find('div', attrs={'class': 'infinity-scroll r-serp__infinity-list'})
+        if main_div:
+            article_list = main_div.find_all('article', attrs={'class': 'vacancy-preview-card'})
+            for div in article_list:
+                card_div = div.find('div', attrs={'class': 'vacancy-preview-card__top'})
+                if card_div:
+                    title = card_div.find('h3', attrs={'class': 'vacancy-preview-card__title'})
+                    print(title.text)
+                    href = domain + title.a['href']
+                    company = 'No name'
+                    cname_1 = card_div.find('span', attrs={'class': 'vacancy-preview-card__company-name'})
+                    if cname_1:
+                        company = cname_1.text
+
+                    content = card_div.find('div', attrs={'class': 'vacancy-preview-card__short-description'}).text
+
+                    jobs.append({'title': title.text.replace('\n            ', ''),
+                                 'url': href,
+                                 'description': content,
+                                 'company': company})
+        else:
+            errors.append({'url': url, 'title': 'Div does not exists'})
+    else:
+        errors.append({'url': url, 'title': 'Page do not response'})
+
+    return jobs, errors
+
+
+if __name__ == '__main__':
+    url = 'https://rostov.rabota.ru/vacancy/?query=python&sort=relevance'
+    jobs, errors = rabota_ru(url)
+    h = codecs.open('work.txt', 'w', 'utf-8')
+    h.write(str(jobs))
+    h.close()
