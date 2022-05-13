f29baf31cac3d7d4b47420c47759d26143d09390
facetable-lab <diofff6@gmail.com>
2022-05-13 04:10:50 +0300

    run_parser v1.1 Updates: - Привязал django к файлу

--- src/run_parser.py
+++ src/run_parser.py
@@ -1,12 +1,20 @@
 import codecs
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'work_search.settings')
+django.setup()
 
 from works_core.parser import *
+from works_core.models import Vacancy, Specialization, City
 
 parsers = (
     (hh_ru, 'https://rostov.hh.ru/search/vacancy?text=python&from=suggest_post&fromSearchLine=true&area=76'),
     (rabota_ru, 'https://rostov.rabota.ru/vacancy/?query=python&sort=relevance'),
     (habr_career, 'https://career.habr.com/vacancies?q=python&l=1&type=all')
 )
+
+city = City.objects.filter(slug='rostov-na-donu')
 
 jobs, errors = [], []
 for func, url in parsers:
