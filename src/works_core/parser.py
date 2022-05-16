152cfc960052ab7756be111d297d6f5053d10a6d
facetable-lab <diofff6@gmail.com>
2022-05-16 14:24:56 +0300

    Парсер v3 Updates: - Проверка на существование url (в городе может не быть вакансий по запросу - Функции парсера принимают два новых параметра для фильтрации: city и specialization, которые она учитывает при поиске

--- src/works_core/parser.py
+++ src/works_core/parser.py
@@ -21,99 +21,109 @@ headers = [{'User-Agent': 'Chrome/91.0.4472.124 Safari/537.36 Edg/91.0.864.59',
            ]
 
 
-def hh_ru(url):
+def hh_ru(url, city=None, specialization=None):
     jobs = []
     errors = []
-    resp = requests.get(url, headers=headers[randint(0, len(headers) - 1)])
-    if resp.status_code == 200:
-        soup = bs(resp.content, 'html.parser')
-        main_div = soup.find('div', attrs={'class': 'vacancy-serp-content'})  # class_={"class": "style"}
-        if main_div:
-            div_list = main_div.find_all('div', attrs={'class': 'vacancy-serp-item'})
-            for div in div_list:
-                title = div.find('h3')
-                href = title.a['href']
-                div_two_span = div.find('div', attrs={'class': 'g-user-content'})
-                content_part_1 = div_two_span.find('div', attrs={'class': 'bloko-text'}).text
-                content_part_2 = div_two_span.find('div', attrs={'class': 'bloko-text bloko-text_no-top-indent'}).text
-                content = content_part_1 + content_part_2
-                company = 'No name'
-                cname_1 = div.find('div', attrs={'class': 'vacancy-serp-item__meta-info-company'})
-                if cname_1:
-                    company = cname_1.a.text
-                jobs.append({'title': title.text,
-                             'url': href,
-                             'description': content,
-                             'company': company})
+    if url:
+        resp = requests.get(url, headers=headers[randint(0, len(headers) - 1)])
+        if resp.status_code == 200:
+            soup = bs(resp.content, 'html.parser')
+            main_div = soup.find('div', attrs={'class': 'vacancy-serp-content'})  # class_={"class": "style"}
+            if main_div:
+                div_list = main_div.find_all('div', attrs={'class': 'vacancy-serp-item'})
+                for div in div_list:
+                    title = div.find('h3')
+                    href = title.a['href']
+                    div_two_span = div.find('div', attrs={'class': 'g-user-content'})
+                    content_part_1 = div_two_span.find('div', attrs={'class': 'bloko-text'}).text
+                    content_part_2 = div_two_span.find('div',
+                                                       attrs={'class': 'bloko-text bloko-text_no-top-indent'}).text
+                    content = content_part_1 + content_part_2
+                    company = 'No name'
+                    cname_1 = div.find('div', attrs={'class': 'vacancy-serp-item__meta-info-company'})
+                    if cname_1:
+                        company = cname_1.a.text
+                    jobs.append({'title': title.text,
+                                 'url': href,
+                                 'description': content,
+                                 'company': company,
+                                 'city_id': city,
+                                 'specialization_id': specialization})
+            else:
+                errors.append({'url': url, 'title': 'Div does not exists'})
         else:
-            errors.append({'url': url, 'title': 'Div does not exists'})
-    else:
-        errors.append({'url': url, 'title': 'Page do not response'})
+            errors.append({'url': url, 'title': 'Page do not response'})
 
     return jobs, errors
 
 
-def rabota_ru(url):
+def rabota_ru(url, city=None, specialization=None):
     domain = 'https://rostov.rabota.ru'
     jobs = []
     errors = []
-    resp = requests.get(url, headers=headers[randint(0, len(headers) - 1)])
-    if resp.status_code == 200:
-        soup = bs(resp.content, 'html.parser')
-        main_div = soup.find('div', attrs={'class': 'infinity-scroll r-serp__infinity-list'})
-        if main_div:
-            article_list = main_div.find_all('article', attrs={'class': 'vacancy-preview-card'})
-            for div in article_list:
-                card_div = div.find('div', attrs={'class': 'vacancy-preview-card__top'})
-                if card_div:
-                    title = card_div.find('h3', attrs={'class': 'vacancy-preview-card__title'})
-                    href = domain + title.a['href']
-                    company = 'No name'
-                    cname_1 = card_div.find('span', attrs={'class': 'vacancy-preview-card__company-name'})
-                    if cname_1:
-                        company = cname_1.text
+    if url:
+        resp = requests.get(url, headers=headers[randint(0, len(headers) - 1)])
+        if resp.status_code == 200:
+            soup = bs(resp.content, 'html.parser')
+            main_div = soup.find('div', attrs={'class': 'infinity-scroll r-serp__infinity-list'})
+            if main_div:
+                article_list = main_div.find_all('article', attrs={'class': 'vacancy-preview-card'})
+                for div in article_list:
+                    card_div = div.find('div', attrs={'class': 'vacancy-preview-card__top'})
+                    if card_div:
+                        title = card_div.find('h3', attrs={'class': 'vacancy-preview-card__title'})
+                        href = domain + title.a['href']
+                        company = 'No name'
+                        cname_1 = card_div.find('span', attrs={'class': 'vacancy-preview-card__company-name'})
+                        if cname_1:
+                            company = cname_1.text
 
-                    content = card_div.find('div', attrs={'class': 'vacancy-preview-card__short-description'}).text
+                        content = card_div.find('div', attrs={'class': 'vacancy-preview-card__short-description'}).text
 
-                    jobs.append({'title': title.text.replace('\n            ', ''),
-                                 'url': href,
-                                 'description': content,
-                                 'company': company})
+                        jobs.append({'title': title.text.replace('\n            ', ''),
+                                     'url': href,
+                                     'description': content,
+                                     'company': company,
+                                     'city_id': city,
+                                     'specialization_id': specialization})
+            else:
+                errors.append({'url': url, 'title': 'Div does not exists'})
         else:
-            errors.append({'url': url, 'title': 'Div does not exists'})
-    else:
-        errors.append({'url': url, 'title': 'Page is empty'})
+            errors.append({'url': url, 'title': 'Page is empty'})
 
     return jobs, errors
 
 
-def habr_career(url):
+def habr_career(url, city=None, specialization=None):
     domain = 'https://career.habr.com/'
     jobs = []
     errors = []
-    resp = requests.get(url, headers=headers[randint(0, len(headers) - 1)])
-    if resp.status_code == 200:
-        soup = bs(resp.content, 'html.parser')
-        main_div = soup.find('div', attrs={'class': 'section-group section-group--gap-medium'})
-        if main_div:
-            div_list = main_div.find_all('div', attrs={'class': 'vacancy-card__info'})
-            for div in div_list:
-                company = 'No name'
-                cname_1 = div.find('div', attrs={'class': 'vacancy-card__company-title'})
-                if cname_1:
-                    company = cname_1.a.text
-                title = div.find('div', attrs={'class': 'vacancy-card__title'}).a
-                href = domain + title['href']
-                content = div.find('div', attrs={'class': 'vacancy-card__skills'}).text
+    if url:
+        resp = requests.get(url, headers=headers[randint(0, len(headers) - 1)])
+        if resp.status_code == 200:
+            soup = bs(resp.content, 'html.parser')
+            main_div = soup.find('div', attrs={'class': 'section-group section-group--gap-medium'})
+            if main_div:
+                div_list = main_div.find_all('div', attrs={'class': 'vacancy-card__info'})
+                for div in div_list:
+                    company = 'No name'
+                    cname_1 = div.find('div', attrs={'class': 'vacancy-card__company-title'})
+                    if cname_1:
+                        company = cname_1.a.text
+                    title = div.find('div', attrs={'class': 'vacancy-card__title'}).a
+                    href = domain + title['href']
+                    content = div.find('div', attrs={'class': 'vacancy-card__skills'}).text
 
-                jobs.append({'title': title.text,
-                             'url': href,
-                             'description': content,
-                             'company': company})
+                    jobs.append({'title': title.text,
+                                 'url': href,
+                                 'description': content,
+                                 'company': company,
+                                 'city_id': city,
+                                 'specialization_id': specialization})
+            else:
+                errors.append({'url': url, 'title': 'Div does not exists'})
         else:
-            errors.append({'url': url, 'title': 'Div does not exists'})
-    else:
-        errors.append({'url': url, 'title': 'Page do not response'})
+            errors.append({'url': url, 'title': 'Page do not response'})
 
     return jobs, errors
 
