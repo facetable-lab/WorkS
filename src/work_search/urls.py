8545ac286ab90395e95c2fecf1ef93490e7ac3f7
facetable-lab <diofff6@gmail.com>
2022-05-18 15:11:17 +0300

    Добавлен новый маршрут для list

--- src/work_search/urls.py
+++ src/work_search/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 
-from works_core.views import home_view
+from works_core.views import *
 
 # Пути приложения
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', home_view),
+    path('', home_view, name='home'),
+    path('list', list_view, name='list'),
 ]
