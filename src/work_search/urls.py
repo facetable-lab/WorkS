a739c12efd32a09dd645f910db900ca0cb64a74a
facetable-lab <diofff6@gmail.com>
2022-05-18 18:59:26 +0300

    Добавлен маршрут к странице авторизации

--- src/work_search/urls.py
+++ src/work_search/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from works_core.views import *
 
 # Пути приложения
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home_view, name='home'),
     path('list', list_view, name='list'),
+    path('accounts/', include(('works_accounts.urls', 'works_accounts'))),
+    path('', home_view, name='home'),
 ]
