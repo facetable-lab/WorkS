0e465cc6e3bfd7926cc4a902d93f896c81623ca1
facetable-lab <diofff6@gmail.com>
2022-05-18 18:59:33 +0300

    Добавлен маршрут к странице авторизации

---
+++ src/works_accounts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from works_accounts.views import *
+
+# Пути приложения
+urlpatterns = [
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+]
